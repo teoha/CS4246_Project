01765ff1d2f89a344df118df3ef5f890d75b992c
yllekjjirpvul98 <kyungchingman@gmail.com>
2020-11-08 15:13:31 +0800

    fix simulate policy

--- agent/__init__.py
+++ agent/__init__.py
@@ -41,21 +41,18 @@ def simulatePolicy(state, model, env):
     Using apprentice model in MCTS simulation for playout.
     '''
     reward = 0.
-    states = []
-    state = torch.FloatTensor(state.state)
+    states = [state]
     isDone = False
     while not isDone:
-        # Get best action at state
-        # print(state.shape)
-        model_state = torch.unsqueeze(state, 0)
+        # Generate next best action with apprentice model
+        tensor_state = torch.FloatTensor(state.state)
+        model_state = torch.unsqueeze(tensor_state, 0)
         actions_q = model.forward(model_state)
         max_q, action = torch.max(actions_q[0],0)
-        states.append(state)
-        state = GridWorldState(state=state[:4].numpy(), is_done=isDone)
-        state = state.simulateStep(env=env,action=action)
-        isDone = state.isDone()
-        state = torch.tensor(state.state)
-        reward += state.getReward()
+        # Transform state to GridWorldState
+        state = GridWorldState(state=state, is_done=isDone)
+        next_state = state.simulateStep(env=env,action=action)
+        states.append(next_state)
     return states
 
 
